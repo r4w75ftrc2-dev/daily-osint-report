e1c0ea0a545173c92856ce76024d5434558f7ae1
r4w75ftrc2-dev <r4w75ftrc2@privaterelay.appleid.com>
2026-08-06 10:27:19 +0200

    Create pdf_report

---
+++ pdf_report.py
@@ -0,0 +1,79 @@
+from reportlab.platypus import (
+    SimpleDocTemplate,
+    Paragraph,
+    Spacer
+)
+
+from reportlab.lib.styles import getSampleStyleSheet
+
+from datetime import datetime
+
+
+def create_pdf():
+
+    date = datetime.now().strftime("%Y-%m-%d")
+
+    filename = (
+        f"reports/"
+        f"Daily_Security_OSINT_Report_{date}.pdf"
+    )
+
+
+    doc = SimpleDocTemplate(
+        filename
+    )
+
+
+    styles = getSampleStyleSheet()
+
+    story = []
+
+
+    with open(
+        "daily_report.md",
+        "r",
+        encoding="utf-8"
+    ) as f:
+
+        content = f.read()
+
+
+    for line in content.split("\n"):
+
+        if line.startswith("#"):
+
+            text = line.replace("#", "").strip()
+
+            story.append(
+                Paragraph(
+                    text,
+                    styles["Heading2"]
+                )
+            )
+
+        else:
+
+            story.append(
+                Paragraph(
+                    line,
+                    styles["BodyText"]
+                )
+            )
+
+
+        story.append(
+            Spacer(1, 8)
+        )
+
+
+    doc.build(story)
+
+
+    print(
+        f"PDF created: {filename}"
+    )
+
+
+if __name__ == "__main__":
+
+    create_pdf()
