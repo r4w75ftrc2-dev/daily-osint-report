be75b14a15c0cb286390519cd44fa10b4b8dfd8a
r4w75ftrc2-dev <r4w75ftrc2@privaterelay.appleid.com>
2026-08-06 09:06:03 +0200

    Add classification configuration

---
+++ config.py
@@ -0,0 +1,43 @@
+CATEGORIES = {
+    "AVIATION": [
+        "airport",
+        "runway",
+        "flight",
+        "aviation",
+        "airline",
+        "aircraft"
+    ],
+
+    "UAV": [
+        "drone",
+        "uav",
+        "uas",
+        "unmanned",
+        "quadcopter"
+    ],
+
+    "SECURITY": [
+        "security",
+        "police",
+        "terrorism",
+        "threat",
+        "attack",
+        "incident"
+    ],
+
+    "GEOPOLITICS": [
+        "war",
+        "military",
+        "nato",
+        "russia",
+        "ukraine",
+        "conflict"
+    ],
+
+    "CZECH": [
+        "czech",
+        "prague",
+        "praha",
+        "czech republic"
+    ]
+}
